3d61175eb49b5a0d6248baf171ca94db0d0ac6c3
Andrew <andrewhubbard78@gmail.com>
2023-09-28 14:29:46 -0400

    A

---
+++ BankEncapsulation/Bank Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class Bank_Account
+    {
+     public Bank_Account()
+        {
+
+        }
+
+        private double balance = 0;
+
+        //Define a method named method that will accept a double
+        //And store that value into the balance feild
+
+        //Encapsulation
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        //Define a method named GetBalance that 
+        //Will return the amount stored in the balance feild
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            var AndrewsAccount = new Bank_Account();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            AndrewsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {AndrewsAccount.GetBalance()}");
+
+
+
+
+
+
+
         }
     }
 }
